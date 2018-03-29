aecabf06cf25c00279f851e1bb9930fed19457ce
roderickzxy <roderickzxy@qq.com>
2018-03-29 09:02:25 +0800

    commit practise code

--- practise2/js/main.js
+++ practise2/js/main.js
@@ -1,8 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        foodList:['hehe1'
-        , 'hehe2', 'hehe3'
+        foodList:[
+            {'name':'香蕉','price':10,'discount':.5},
+            {'name':'苹果','price':20,'discount':.3},
+            {'name':'菠萝','price':14,'discount':null},
         ]
     }
 });
